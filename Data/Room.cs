4856c54c903b3abae36e1cdb4efab403b55d50c9
holiman123 <luchikholiday@gmail.com>
2025-12-23 00:20:30 +0000

    Now room remembers a time of the video. People connecting while video is playing will stop video for all users in room and load current video time. So nobody would be desynced.

--- Data/Room.cs
+++ Data/Room.cs
@@ -12,5 +12,33 @@ namespace WatchTogether3.Data
         public List<string> UploadedVideoUrls { get; set; } = new List<string>();
 
         public ApplicationUser Owner { get; set; }
+
+        public bool IsPlaying { get; set; }
+
+        public double CurrentTime
+        {
+            get
+            {
+                if (IsPlaying)
+                {
+                    double timeSinceLastPlay = (DateTime.Now - LastPlayTime).TotalSeconds;
+                    return field + timeSinceLastPlay;
+                }
+                else
+                {
+                    return field;
+                }
+            }
+
+            set => field = value;
+        }
+
+        /// <summary>
+        /// Time of the last play action. <br/>
+        /// Used to calculate the current time when the video is playing.
+        /// </summary>
+        public DateTime LastPlayTime { get; set; }
+
+        public DateTime CreationDate { get; set; }
     }
 }
---
+++ Migrations/20251222211452_mssql.local_migration_567.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WatchTogether3.Migrations
+{
+    /// <inheritdoc />
+    public partial class mssqllocal_migration_567 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreationDate",
+                table: "Rooms",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<double>(
+                name: "CurrentTime",
+                table: "Rooms",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPlaying",
+                table: "Rooms",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastPlayTime",
+                table: "Rooms",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreationDate",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "CurrentTime",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "IsPlaying",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "LastPlayTime",
+                table: "Rooms");
+        }
+    }
+}
